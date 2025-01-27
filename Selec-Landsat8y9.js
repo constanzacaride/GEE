4af58a4450047e43322f7c2b07f1505b41e7b23f
constanzacaride <146881426+constanzacaride@users.noreply.github.com>
2025-01-27 11:00:29 -0300

    Update Selec-Landsat8y9.js

--- Selec-Landsat8y9.js
+++ Selec-Landsat8y9.js
@@ -1,4 +1,5 @@
-//Elegir la zona de interés (geometry) -> Se incorporará arriba como variable
+//OPCIÓN A: Elegir la zona de interés (geometry) -> Se incorporará arriba como variable
+//OPCIÓN B: Si tengo un shapefile en los assets lo abro y le cambio el nombre a geometry.
 
 //Elegir la ventana de tiempo que quiero explorar 
 var Inicio = '2024-09-20';
@@ -6,28 +7,29 @@ var Fin = '2024-09-30';
 
 //Busca todas las imgs L8 disponibles para ese sitio entre esas fechas
 var L8 = ee.ImageCollection("LANDSAT/LC08/C02/T1_L2")
+.filterBounds(geometry)
+.filterDate(Inicio, Fin);
 //.filter(ee.Filter.eq('WRS_PATH', 44))
 //.filter(ee.Filter.eq('WRS_ROW', 34))
 //.filter(ee.Filter.lt('CLOUD_COVER', 20))
-.filterDate(Inicio, Fin)
-.filterBounds(geometry);
 
 //Busca todas las imgs L9 disponibles para ese sitio entre esas fechas
 var L9 = ee.ImageCollection("LANDSAT/LC09/C02/T1_L2")
+.filterBounds(geometry)
+.filterDate(Inicio, Fin);
 //.filter(ee.Filter.eq('WRS_PATH', 44))
 //.filter(ee.Filter.eq('WRS_ROW', 34))
 //.filter(ee.Filter.lt('CLOUD_COVER', 20))
-.filterDate(Inicio, Fin)
-.filterBounds(geometry);
 
 //Imprimo las imágenes en la consola
 print(L8);
 print(L9);
 
 //Parámetros de visualización
-var pvL89c2tc = {"opacity":1,"bands":["SR_B4","SR_B3","SR_B2"],"min":7230,"max":41434,"gamma":1}; 
-var pvL89c2veg = {"opacity":1,"bands":["SR_B5","SR_B6","SR_B4"],"min":7230,"max":41434,"gamma":1}; //IRC, IRM, Rojo
-var pvL89c2fu = {"opacity":1,"bands":["SR_B7","SR_B5","SR_B2"],"min":7230,"max":41434,"gamma":1}; //IRM, IRC, Azul
+var pvL89c2tc = {"opacity":1,"bands":["SR_B4","SR_B3","SR_B2"],"min":7230,"max":41434,"gamma":1}; //True color R, G, B
+var pvL89c2veg = {"opacity":1,"bands":["SR_B5","SR_B6","SR_B4"],"min":7230,"max":41434,"gamma":1}; // Vegetación IRC, IRM, Rojo
+var pvL89c2fu = {"opacity":1,"bands":["SR_B7","SR_B5","SR_B2"],"min":7230,"max":41434,"gamma":1}; //Fuego IRM, IRC, Azul
+var pvS2ag = {"opacity":1,"bands":['B12','B11','B4'],'min': 0, 'max': 3000,gamma:1}; //Agua SWIR 2, SWIR 1, Rojo
 
 //Función que despliega las imágnes en el mapa (si quiero otra combinación de badas cambio los param de vis)
 function addImageL8(image) { 
